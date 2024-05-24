260848673b114154a457d1fee0893e48a93d8b86
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-24 22:32:52 +0300

    D2

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JUSTATARI/Matrix-Music",
+    "https://github.com/JUSTATARI/Resx-Music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
