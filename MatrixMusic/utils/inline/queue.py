08c64167e0df8d7e9797b973f3f6e82bea125426
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-27 01:58:40 +0300

    D2

--- MatrixMusic/utils/inline/queue.py
+++ MatrixMusic/utils/inline/queue.py
@@ -61,15 +61,3 @@ def queue_back_markup(_, CPLAY):
         ]
     )
     return upl
-
-
-def aq_markup(_, chat_id):
-    buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-    ]
-    return buttons
