1b18de0fe25ea6451afe582fe2aadb57815172f4
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-27 02:22:57 +0300

    D2

--- MatrixMusic/utils/stream/stream.py
+++ MatrixMusic/utils/stream/stream.py
@@ -10,7 +10,7 @@ from MatrixMusic.core.call import Zelzaly
 from MatrixMusic.misc import db
 from MatrixMusic.utils.database import add_active_video_chat, is_active_chat
 from MatrixMusic.utils.exceptions import AssistantErr
-from MatrixMusic.utils.inline import aq_markup, close_markup, stream_markup
+from MatrixMusic.utils.inline import close_markup, stream_markup
 from MatrixMusic.utils.pastebin import ZelzalyBin
 from MatrixMusic.utils.stream.queue import put_queue, put_queue_index
 from MatrixMusic.utils.thumbnails import get_thumb
