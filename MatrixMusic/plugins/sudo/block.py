9bf3ab3ff8b33244bdf43b3c4bf854b7da5209a1
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-28 01:59:33 +0300

    D2

--- MatrixMusic/plugins/sudo/block.py
+++ MatrixMusic/plugins/sudo/block.py
@@ -6,7 +6,6 @@ from MatrixMusic import app
 from MatrixMusic.misc import SUDOERS
 from MatrixMusic.utils.database import add_gban_user, remove_gban_user
 from MatrixMusic.utils.decorators.language import language
-from MatrixMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
