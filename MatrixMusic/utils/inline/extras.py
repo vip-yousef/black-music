23e7c117c221fcf6b739b82db83e9c2f34f456ed
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-29 04:02:45 +0300

    D2

--- MatrixMusic/utils/inline/extras.py
+++ MatrixMusic/utils/inline/extras.py
@@ -5,9 +5,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["PL_B_1"],callback_data="get_playlist_playmode"),
-            InlineKeyboardButton(text=_["PL_B_8"], callback_data="get_top_playlists"),
-            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
