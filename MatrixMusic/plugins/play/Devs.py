0b0f31b6ae99ee5a0992d182f7f8a53f06ca6c4e
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-31 03:41:04 +0300

    D2

--- MatrixMusic/plugins/play/Devs.py
+++ MatrixMusic/plugins/play/Devs.py
@@ -62,7 +62,7 @@ async def devatari(_, query: CallbackQuery):
                                      f"<b>≭︰اليوزر @{user_ab}\n</b>"
                                      f"<b>≭︰ايدي المجموعة {chat}\n</b>")
     
-    await query.message.reply_text(f"<b>≭︰تم إرسال النداء إلى مطور البوت\n\n↯︙Dᥱꪜ - @{usm} .</b>")
+    await query.message.reply_text(f"<b>≭︰تم إرسال النداء إلى مطور البوت\n\n↯︙Dᥱꪜ - @A1RTR .</b>")
 
 @app.on_message(
     command(["المطور"])
