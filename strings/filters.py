3299a2e7b9c4b188911e42f7be3ca9144599d22a
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-05-31 17:31:03 +0300

    D2

--- strings/filters.py
+++ strings/filters.py
@@ -3,7 +3,7 @@ from typing import List, Union
 from pyrogram import filters
 
 
-other_filters = filters.group  & ~filters.via_bot & ~filters.forwarde
+other_filters = filters.group  & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
     filters.private  & ~filters.via_bot & ~filters.forwarded & ~filters.regex
 )
