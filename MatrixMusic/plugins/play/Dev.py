4526581d985a1482d1e3be5d7a49bb36de3d0b74
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-06-02 03:19:43 +0300

    Update Dev.py

--- MatrixMusic/plugins/play/Dev.py
+++ MatrixMusic/plugins/play/Dev.py
@@ -98,18 +98,23 @@ async def rsexs(client, message):
                              )
 
 
-@app.on_message(command(["تخ"]) & filters.group)
-async def huhh(client, message):
-    await message.reply_animation(
-        animation=f"https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
-        caption=f"""≭︰قتل ↫ ⦗ {message.from_user.mention} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ [{user.first_name}](tg://user?id={user.id}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
-    )
-    reply_markup=InlineKeyboardMarkup(
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-       [
-           [
-               InlineKeyboardButton(
-                   "‹ 𝐁𝐥𝐚𝐜𝐤 𝐓𝐞𝐀𝐦 ›", url=f"https://t.me/vvizinn"),
-           ],
-       ]
-    ),
+app = Client("my_account")
+
+@app.on_message(filters.command(["تخ"]) & filters.group)
+async def huhh(client, message):
+    user = message.from_user
+    await message.reply_animation(
+        animation="https://telegra.ph/file/5a18fe591860a8a98f39f.mp4",
+        caption=f"""≭︰قتل ↫ ⦗ {message.from_user.mention} ⦘\n≭︰الضحيه دا 😢 ↫ ⦗ [{user.first_name}](tg://user?id={user.id}) ⦘\nانا لله وانـا اليـه راجعـون 😢😢""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+               [
+                   InlineKeyboardButton(
+                       "‹ 𝐁𝐥𝐚𝐜𝐤 𝐓𝐞𝐀𝐦 ›", url="https://t.me/vvizinn"),
+               ],
+           ]
+        )
+    )
