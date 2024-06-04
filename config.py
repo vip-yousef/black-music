fec9c07653ee626547dc1f63ac646bb1a72b0303
𝖺𝖳𝖺𝗋𝗂 <ssdcv608@gmail.com>
2024-06-04 19:14:43 +0300

    D2

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/JUSTATARI/VBTB",
+    "https://github.com/afkhcftjma/Vxxyijwepp",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
