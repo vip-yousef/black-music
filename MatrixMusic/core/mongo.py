5cc0133097d3f95a904b5d71b817661ed48ce972
⏤͟͞ َِ⍣⃟َ𝐓𝐇ِ𝐄 𓆩ََِِ𝐘َِْ𝐎ِِ𝐔ًٌُ𝐒𝐄ُِ𝐅𓆪 <167021222+yousef1yt@users.noreply.github.com>
2024-06-05 03:55:56 +0300

    Update mongo.py

--- MatrixMusic/core/mongo.py
+++ MatrixMusic/core/mongo.py
@@ -4,11 +4,11 @@ from config import MONGO_DB_URI
 
 from ..logging import LOGGER
 
-LOGGER("ميــوزك ماتـركس").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
+LOGGER("ميــوزك بلاك").info("جـارِ الاتصـال بقاعـدة البيانـات . . .")
 try:
     _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.Anon
-    LOGGER("ميــوزك ماتـركس").info("تم الاتصـال بقاعـدة البيانـات ...✓")
+    LOGGER("ميــوزك بلاك").info("تم الاتصـال بقاعـدة البيانـات ...✓")
 except:
     LOGGER(__name__).error("حدث خطأ اثناء الاتصال بقاعدة البيانات.")
     exit()
